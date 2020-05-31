00d62e2f95ad96ee11950cf20180890a9f758166
TK Tran <tktrannn@live.com>
2020-05-31 00:01:24 -0400

    Generated LoginComponent and RegisterComponent to UI mockup. Everything is displayed on one template; no routing implemented yet.

--- do-stuff-app/src/app/app.module.ts
+++ do-stuff-app/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+// ROOT MODULE
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
