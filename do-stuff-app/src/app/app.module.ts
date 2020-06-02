6aa62e16ca5ad64e997c32a49f9924b9855b0a4d
TK Tran <tktrannn@live.com>
2020-06-01 22:43:16 -0400

    Created super rough draft of ProfileComponent and EventComponent.

--- do-stuff-app/src/app/app.module.ts
+++ do-stuff-app/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EventComponent } from './event/event.component';
 
 // ROOT MODULE
 @NgModule({
 	declarations: [
 		AppComponent,
 		LoginComponent,
-		RegisterComponent
+		RegisterComponent,
+		ProfileComponent,
+		EventComponent
 	],
 	imports: [
 		BrowserModule,
