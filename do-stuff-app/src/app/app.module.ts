fa0056596b64494a8ba26c78342e0789bf067efb
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-06-02 01:21:55 -0400

    added some basic routing for dev tomorrow

--- do-stuff-app/src/app/app.module.ts
+++ do-stuff-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EventComponent } from './event/event.component';
+import { RouterModule } from '@angular/router';
 
 // ROOT MODULE
 @NgModule({
@@ -19,7 +21,15 @@ import { EventComponent } from './event/event.component';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		FormsModule,
+		AppRoutingModule,
+
+		RouterModule.forRoot([
+			{path: 'login', component: LoginComponent},
+			{path: 'register', component: RegisterComponent},
+			{path: 'profile', component: ProfileComponent},
+			{path: 'event', component: EventComponent},
+		  ])
 	],
 	providers: [],
 	bootstrap: [AppComponent]
