1f7a1569a1d06db1f5faa3c90409ebea2482a8b1
TK Tran <tktrannn@live.com>
2020-06-04 17:45:12 -0400

    Successfully implemented routing with hard-coded email and password.

--- GatherUp/src/main/webapp/do-stuff-app/src/app/app-routing.module.ts
+++ GatherUp/src/main/webapp/do-stuff-app/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EventComponent } from './event/event.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
+	{ path: 'login', component: LoginComponent },
 	{ path: 'profile', component: ProfileComponent },
 	{ path: 'event', component: EventComponent }
 ];
--- GatherUp/src/main/webapp/do-stuff-app/src/app/register/register.component.ts
+++ GatherUp/src/main/webapp/do-stuff-app/src/app/register/register.component.ts
@@ -3,9 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from "../class/user/user"; // imports our User class
 
 /* 
-	This component maps input values to a user object and passes it to Java
+	This component maps input values to a JavaScript object and passes it to Java via Angular's HttpClient
 */
-
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
@@ -19,16 +18,24 @@ export class RegisterComponent implements OnInit {
 	ngOnInit(): void {
 	}
 
+	// Angular yells if you remove this..
+	user = new User(null, null, null, null, null);
+
 	onSubmit() {
 		console.log("Register submit button pressed.");
 
-		let email = document.getElementById("inputEmail").value;
-		let password = document.getElementById("inputPassword").value;
-		let firstName = document.getElementById("inputFirstName").value;
-		let lastName = document.getElementById("inputLastName").value;
-		let contact = document.getElementById("inputContact").value;
+		let email: any = (<HTMLInputElement>document.getElementById("inputEmail")).value;
+		let password: any = (<HTMLInputElement>document.getElementById("inputPassword")).value;
+		let firstName: any = (<HTMLInputElement>document.getElementById("inputFirstName")).value;
+		let lastName: any = (<HTMLInputElement>document.getElementById("inputLastName")).value;
+		let contact: any = (<HTMLInputElement>document.getElementById("inputContact")).value;
 		let user = new User(email, password, firstName, lastName, Number(contact));
 
+		if (user.email == "hello@world.com" && user.password == "p4ssw0rd") {
+			window.location.href = "/profile";
+		} else {
+			window.location.href = "https://www.google.com";
+		}
 		console.log(user);
 	}
 }
