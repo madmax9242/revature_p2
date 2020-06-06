27610781d6769c5de4da9d168b7bc9f5dcffa71e
Dave_Wroblewski <dwroble1@gmail.com>
2020-06-05 20:55:35 -0400

    bare project

---
+++ gatherup-backend/src/test/java/com/gatherup/app/GatherupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatherup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
