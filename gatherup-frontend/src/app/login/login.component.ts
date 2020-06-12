1e55b26d83a90948c48492f9047c1ec7f317713c
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-06-12 00:42:28 -0400

    test destroy

--- gatherup-frontend/src/app/login/login.component.ts
+++ gatherup-frontend/src/app/login/login.component.ts
@@ -2,50 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { User } from '../class/user/user';
-import { ConfigService } from '../service/config.service';
-import { PasswordEncryptionService } from '../service/password-encryption.service';
 
-@Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit {
 
-	user: User;
-	tempUser: User;
+		
 
-	constructor(private router: Router, private configService: ConfigService, private encryptionService: PasswordEncryptionService) {
-		this.user = new User(undefined, undefined, undefined, undefined, undefined, undefined);
-	}
-
-	ngOnInit(): void {
-
-	}
-
-	login() {
-		console.log("login() button pressed.");
-
-		// Constructs a user object
-		let email = (<HTMLInputElement>document.getElementById("inputEmail")).value;
-		let password = (<HTMLInputElement>document.getElementById("inputPassword")).value;
-		// this.user = new User(undefined, email, this.encryptionService.encrypt(password), undefined, undefined, undefined);
-		this.user.email = email;
-		this.user.password = password;
-
-		// Sanity check
-		console.log("Sending user to BE: ");
-		console.log(this.user);
-
-		this.configService.login(this.user).subscribe(data => { this.tempUser = data });
-		console.log("User received from DB: ");
-		console.log(this.tempUser);
-
-		if (this.tempUser != null) {
-			this.router.navigate(["/eventview"]);
-		} else {
-			alert("Invalid credentials!");
-		}
+		// if (this.tempUser != null) {
+		// 	this.router.navigate(["/eventview"]);
+		// } else {
+		// 	alert("Invalid credentials!");
+		// }
 	}
 }
 
