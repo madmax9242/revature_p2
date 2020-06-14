dd10e820579247ea0084c171b5323454f3857dba
Dave_Wroblewski <dwroble1@gmail.com>
2020-06-14 11:27:45 -0400

    added logging

--- gatherup-backend/src/main/java/com/gatherup/app/service/impl/EventServiceImpl.java
+++ gatherup-backend/src/main/java/com/gatherup/app/service/impl/EventServiceImpl.java
@@ -3,6 +3,8 @@ package com.gatherup.app.service.impl;
 import com.gatherup.app.dao.EventDao;
 import com.gatherup.app.model.Event;
 import com.gatherup.app.service.EventService;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +17,29 @@ public class EventServiceImpl implements EventService {
 	@Autowired
 	private EventDao eventDao;
 
+	//For information gathering
+	private Logger infoLogger = Logger.getLogger("InfoLogger");
+	//For error reporting
+	private Logger errorLogger = Logger.getLogger("ErrorLogger");
+		
 	// CREATE
 	@Override
 	public Event createEvent(Event event) {
+		infoLogger.info(event.getEventName() + " was just created.");
 		return eventDao.save(event);
 	}
 
 	// UPDATE
 	@Override
 	public Event updateEvent(Event event) {
+		infoLogger.info(event.getEventName() + " was just updated.");
 		return eventDao.save(event);
 	}
 
 	// DELETE
 	@Override
 	public void deleteEventById(int id) {
+		infoLogger.info(getEventById(id).getEventName() + " was just deleted.");
 		eventDao.deleteById(id);
 	}
 
