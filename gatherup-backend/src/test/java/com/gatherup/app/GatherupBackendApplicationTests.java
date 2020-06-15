955573707bc77d3b167a05932decb672c6efd9e1
Dave_Wroblewski <dwroble1@gmail.com>
2020-06-15 11:29:06 -0400

    Added JUnit Testing

--- gatherup-backend/src/test/java/com/gatherup/app/GatherupBackendApplicationTests.java
+++ gatherup-backend/src/test/java/com/gatherup/app/GatherupBackendApplicationTests.java
@@ -3,27 +3,48 @@ package com.gatherup.app;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.gatherup.app.model.Event;
 import com.gatherup.app.model.User;
 import com.gatherup.app.service.UserService;
+import com.gatherup.app.service.impl.EventServiceImpl;
 import com.gatherup.app.service.impl.UserServiceImpl;
 
 @SpringBootTest
 class GatherupBackendApplicationTests {
+	
+	@Autowired
+	private UserServiceImpl user;
+	@Autowired
+	private EventServiceImpl event;
 
 	@Test
 	void contextLoads() {
+		System.out.println("I am in TEST!");
+		
 	}
 	
-//	@Test
-//	void userTable() {
-//		UserServiceImpl service = new UserServiceImpl();
-//		User daveByEmail = service.getUserByEmail("dwroble1@gmail.com");
-//		//User daveById = service.getUserById(0);
-//		
-//		assertEquals("Dave", daveByEmail.getFirstName());
-//		//assertEquals("Dave", daveById.getFirstName());
-//	}
+	@Test
+	void userTable() {
+		User userByEmail = user.getUserByEmail("dwroble2@gmail.com");
+
+		assertEquals("5853970486", userByEmail.getContact());
+		assertEquals("dwroble2@gmail.com", userByEmail.getEmail());
+		assertEquals("Dan", userByEmail.getFirstName());
+		assertEquals("Wisniskski", userByEmail.getLastName());
+	}
+	
+	@Test
+	void eventsTable() {
+		Event eventByName = event.getEventById(1);
+		
+		assertEquals("06/04/2020 13:30", eventByName.getDateTime());
+		assertEquals("Picnic, bring food", eventByName.getEventDescription());
+		assertEquals("Ellison Park", eventByName.getEventLocation());
+		assertEquals("4th of July Picnic", eventByName.getEventName());
+		assertEquals("Gathering", eventByName.getEventType());
+	}
 
 }
